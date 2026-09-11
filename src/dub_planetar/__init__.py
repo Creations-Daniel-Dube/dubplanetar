b3247cdb6006ab8df89229ed6043c1dc69281134
Daniel Dubé <daniel.dube99@gmail.com>
2026-09-10 23:20:17 -0400

    v0.8.200

--- src/dub_planetar/__init__.py
+++ src/dub_planetar/__init__.py
@@ -1,7 +1,7 @@
 #***********************************************
 #* (c) Créations Daniel Dubé     Daniel Dubé   *
-#* Dernières Modifications -->   2026-09-03    *
+#* Dernières Modifications -->   2026-09-10    *
 #***********************************************
 """DubPlanetar — empilement GPU Soleil/Lune pour vidéos RAW SeeStar."""
 
-__version__ = "0.8.150"
+__version__ = "0.8.200"
